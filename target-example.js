68c20c66ab20f608eb2249e2639f544cf7f13792
jxmot <webdev@stupiddog.net>
2021-05-17 15:50:46 -0500

    initial commit, target options example file

---
+++ target-example.js
@@ -0,0 +1,10 @@
+'use strict';
+module.exports = {
+    target:'https://example.com/',
+    fullpage:true,
+    // these are optional, and can be omitted or
+    // commented out. The folders in imgpath must
+    // exist prior to running the app.
+    imgpath:'./screenshots/',   // default = ./  but MUST end with '/'!
+    imgextn:'.png'              // default .png
+};
